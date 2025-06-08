73446f917619d68213d74dc7e0e8e2de0abf5237
mriskisyahputraa2 <riskideveloper2@gmail.com>
2025-06-08 22:02:18 +0700

    [FEAT] UPDATE MIGRATIONS USER

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,11 +16,8 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->unsignedBigInteger('role_id')->nullable();
-            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('set null'); // Foreign key ke tabel roles
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
